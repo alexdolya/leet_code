e68a5b7895eb6ab3db1acc82820d1629d6443e4d
Alexey Dolya <alexdolya90@gmail.com>
2024-03-14 07:48:47 +0400

    problem 1518 solved

---
+++ src/main/java/ru/dolya/easy/problem1518/Solution.java
@@ -0,0 +1,25 @@
+package ru.dolya.easy.problem1518;
+
+
+class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.numWaterBottles(9, 3));
+        System.out.println(solution.numWaterBottles(15, 4));
+
+    }
+
+    public int numWaterBottles(int numBottles, int numExchange) {
+        return numWaterBottlesWithEmpty(numBottles, numExchange, 0);
+    }
+
+    private int numWaterBottlesWithEmpty(int numBottles, int numExchange, int emptyBottles) {
+        if (numBottles == 0 && emptyBottles / numExchange == 0) {
+            return 0;
+        }
+        return numBottles + emptyBottles / numExchange
+                + numWaterBottlesWithEmpty(0, numExchange, numBottles + emptyBottles/numExchange + emptyBottles% numExchange);
+    }
+
+}
