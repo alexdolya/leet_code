23f38b7d8fee396dd558138e1bfe6aa41be315d8
alexdolya <alexdolya90@gmail.com>
2025-10-08 09:35:24 +0400

    problem 283 second solution

--- src/main/java/ru/dolya/easy/problem283/Solution.java
+++ src/main/java/ru/dolya/easy/problem283/Solution.java
@@ -8,9 +8,27 @@ public class Solution {
         int[] nums = {0, 1, 0, 3, 12};
         moveZeroes(nums);
         System.out.println(Arrays.toString(nums));
+        int[] nums2 = {1, 0};
+        moveZeroes(nums2);
+        System.out.println(Arrays.toString(nums2));
     }
 
     public static void moveZeroes(int[] nums) {
+        if (nums.length == 0 || nums.length == 1) {
+            return;
+        }
+        int zeroPointer = 0;
+        for (int nonZeroPointer = 0; nonZeroPointer < nums.length; nonZeroPointer++) {
+            if (nums[nonZeroPointer] != 0) {
+                int temp = nums[nonZeroPointer];
+                nums[nonZeroPointer] = nums[zeroPointer];
+                nums[zeroPointer] = temp;
+                zeroPointer++;
+            }
+        }
+    }
+
+    public static void moveZeroes2(int[] nums) {
         int[] ints = new int[nums.length];
         for (int i = 0, j = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
