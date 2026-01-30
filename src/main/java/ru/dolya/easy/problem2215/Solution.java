ef5af26c12344819c144cedcb1f1d3664f2752a1
alexdolya <alexdolya90@gmail.com>
2026-01-30 12:56:35 +0400

    problem 2215 solved

---
+++ src/main/java/ru/dolya/easy/problem2215/Solution.java
@@ -0,0 +1,38 @@
+package ru.dolya.easy.problem2215;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        Set<Integer> set2 = new HashSet<>();
+        for (int num : nums2) {
+            set2.add(num);
+        }
+
+        Set<Integer> result1 = new HashSet<>();
+
+        for (int num : nums1) {
+            if (!set2.contains(num)){
+                result1.add(num);
+            }
+        }
+
+        Set<Integer> result2 = new HashSet<>();
+
+        for (int num : nums2) {
+            if (!set1.contains(num)){
+                result2.add(num);
+            }
+        }
+        return List.of(new ArrayList<>(result1), new ArrayList<>(result2));
+    }
+}
