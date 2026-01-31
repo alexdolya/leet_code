0ea126439b256c97156556f9c2336771e91c722e
alexdolya <alexdolya90@gmail.com>
2026-01-31 22:01:51 +0400

    problem 104 solved

---
+++ src/main/java/ru/dolya/easy/problem104/Solution.java
@@ -0,0 +1,52 @@
+package ru.dolya.easy.problem104;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int result = 0;
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+            result++;
+        }
+        return result;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
