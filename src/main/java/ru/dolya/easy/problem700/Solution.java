3b63f0b1bea7f25d499390d55f14ed27212ac78d
alexdolya <alexdolya90@gmail.com>
2026-01-31 22:07:31 +0400

    problem 700 solved

---
+++ src/main/java/ru/dolya/easy/problem700/Solution.java
@@ -0,0 +1,39 @@
+package ru.dolya.easy.problem700;
+
+public class Solution {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        }
+
+        if (val > root.val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
